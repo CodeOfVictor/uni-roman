4214f0064bd86742220d79d9829815de5d91bb2d
CodeOfVictor <60798818+CodeOfVictor@users.noreply.github.com>
2023-05-13 18:27:59 +0200

    Update tests.js fixing bug

--- tests.js
+++ tests.js
@@ -43,38 +43,38 @@ test('all', function (assert) {
   assert.propEqual(result8, expected8, 'Test Case 8 with integer that has a 9');
   
   // Test case 9
-  const result1 = convertRomanToInteger('I');
-  const expected1 = { value: 1, message: '', result: true };
+  const result9 = convertRomanToInteger('I');
+  const expected9 = { value: 1, message: '', result: true };
   assert.propEqual(result1, expected1, 'Test Case 1');
 
   // Test case 10
-  const result2 = convertRomanToInteger('IX');
-  const expected2 = { value: 9, message: '', result: true };
+  const result10 = convertRomanToInteger('IX');
+  const expected10 = { value: 9, message: '', result: true };
   assert.propEqual(result2, expected2, 'Test Case 2');
 
   // Test case 11
-  const result3 = convertRomanToInteger('XLIX');
-  const expected3 = { value: 49, message: '', result: true };
+  const result11 = convertRomanToInteger('XLIX');
+  const expected11 = { value: 49, message: '', result: true };
   assert.propEqual(result3, expected3, 'Test Case 3');
 
   // Test case 12
-  const result4 = convertRomanToInteger('MCMLXXXIV');
-  const expected4 = { value: 1984, message: '', result: true };
+  const result12 = convertRomanToInteger('MCMLXXXIV');
+  const expected12 = { value: 1984, message: '', result: true };
   assert.propEqual(result4, expected4, 'Test Case 4');
 
   // Test case 13
-  const result5 = convertRomanToInteger('MMMCMXCIX');
-  const expected5 = { value: 3999, message: '', result: true };
+  const result13 = convertRomanToInteger('MMMCMXCIX');
+  const expected13 = { value: 3999, message: '', result: true };
   assert.propEqual(result5, expected5, 'Test Case 5');
 
   // Test case 14
-  const result6 = convertRomanToInteger('ABC');
-  const expected6 = { value: 0, message: 'Invalid Roman numeral', result: false };
+  const result14 = convertRomanToInteger('ABC');
+  const expected14 = { value: 0, message: 'Invalid Roman numeral', result: false };
   assert.propEqual(result6, expected6, 'Test Case 6');
 
   // Test case 15
-  const result7 = convertRomanToInteger('');
-  const expected7 = { value: 0, message: 'Empty input', result: false };
+  const result15 = convertRomanToInteger('');
+  const expected15 = { value: 0, message: 'Empty input', result: false };
   assert.propEqual(result7, expected7, 'Test Case 7');
 });
 
